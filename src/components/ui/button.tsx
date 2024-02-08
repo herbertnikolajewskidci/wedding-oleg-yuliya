732b7e55ab596a50649ead4fbc73da3897910ebe
Herbert Nikolajewski <114996264+herbertnikolajewskidci@users.noreply.github.com>
2024-02-08 11:28:42 +0100

    button.tsx aktualisieren

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,11 +9,11 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90 active:bg-accent",
+        default: "bg-primary text-primary-foreground hover:bg-primary/90 active:bg-accent active:text-accent-foreground",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent active:bg-accent active:text-accent-foreground hover:text-accent-foreground",
+          "border border-input bg-background hover:bg-accent active:bg-secondary active:text-secondary-foreground hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
