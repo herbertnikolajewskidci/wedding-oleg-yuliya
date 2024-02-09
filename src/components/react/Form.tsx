f2119cc37c4b97def69d055945531fc784c59266
herbertnikolajewskidci <herbert.nikolajewski@digitalcareerinstitute.org>
2024-02-09 15:26:45 +0100

    Füge Toaster-Komponente hinzu und aktualisiere Bestellungsdatenstruktur

--- src/components/react/Form.tsx
+++ src/components/react/Form.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Toaster } from "@/components/ui/toaster";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
@@ -15,6 +16,12 @@ const FormSchema = z.object({
         message: "Bitte gib mindestens einen Namen an.",
     }),
     unterkunft: z.boolean(),
+    essen: z.array(
+        z.object({
+            name: z.string(),
+            anzahl: z.number().int().min(0),
+        })
+    ),
 });
 
 export function InputForm() {
@@ -23,6 +30,11 @@ export function InputForm() {
         defaultValues: {
             username: "",
             unterkunft: false,
+            essen: [
+                { name: "Schnitzel", anzahl: 0 },
+                { name: "Schweinebraten", anzahl: 0 },
+                { name: "Hähnchen", anzahl: 0 },
+            ],
         },
     });
 
@@ -45,85 +57,95 @@ export function InputForm() {
         { name: "Hähnchenschenkel", imgSrc: "/haehnchen.jpeg" },
     ];
 
-    const [bestellungen, setBestellungen] = useState(
-        Array(gerichte.length).fill(0)
-    );
+    const [bestellungen, setBestellungen] = useState([
+        { name: "Schnitzel", anzahl: 0 },
+        { name: "Schweinebraten", anzahl: 0 },
+        { name: "Hähnchen", anzahl: 0 },
+    ]);
 
     const handleAnzahlAendern = (index: number, anzahl: number) => {
         const neueBestellungen = [...bestellungen];
-        neueBestellungen[index] = anzahl;
+        neueBestellungen[index].anzahl = anzahl;
         setBestellungen(neueBestellungen);
     };
 
     return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-                <div>
-                    <FormField
-                        control={form.control}
-                        name="username"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormControl>
-                                    <Input
-                                        placeholder="Name bzw. Familie..."
-                                        {...field}
-                                        className="bg-white"
-                                    />
-                                </FormControl>
-                            </FormItem>
-                        )}
-                    />
-                </div>
+        <>
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-6"
+                >
+                    <div>
+                        <FormField
+                            control={form.control}
+                            name="username"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <Input
+                                            placeholder="Name bzw. Familie..."
+                                            {...field}
+                                            className="bg-white"
+                                        />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                    </div>
 
-                <div className="flex items-center space-x-3">
-                    <input
-                        type="checkbox"
-                        name="unterkunft"
-                        id="unterkunft"
-                        className="w-8 h-8 text-primary  border-secondary rounded focus:ring-primary dark:focus:ring-primary dark:ring-offset-primary focus:ring-2 dark:bg-primary dark:border-secondary"
-                    />
-                    <label
-                        htmlFor="unterkunft"
-                        className="font-bold leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                    >
-                        Ich benötige eine Übernachtungsmöglichkeit
-                    </label>
-                </div>
-                <div className="flex flex-col gap-2">
-                    {gerichte.map((gericht, index) => (
-                        <div
-                            key={index}
-                            className="flex justify-between items-center gap-4 flex-nowrap break-words"
+                    <div className="flex items-center space-x-3">
+                        <input
+                            type="checkbox"
+                            name="unterkunft"
+                            id="unterkunft"
+                            className="w-8 h-8 text-primary  border-secondary rounded focus:ring-primary dark:focus:ring-primary dark:ring-offset-primary focus:ring-2 dark:bg-primary dark:border-secondary"
+                        />
+                        <label
+                            htmlFor="unterkunft"
+                            className="font-bold leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                         >
-                            <img
-                                src={gericht.imgSrc}
-                                alt={gericht.name}
-                                className="w-28 h-28 rounded-lg object-cover"
-                            />
-                            <label className="text-md">{gericht.name}</label>
-                            <input
-                                type="tel"
-                                className="w-14 h-14 rounded-lg pl-3"
-                                value={bestellungen[index]}
-                                placeholder="0"
-                                onChange={(e) =>
-                                    handleAnzahlAendern(
-                                        index,
-                                        Number(e.target.value)
-                                    )
-                                }
-                            />
-                        </div>
-                    ))}
-                </div>
+                            Ich benötige eine Übernachtungsmöglichkeit
+                        </label>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        {gerichte.map((gericht, index) => (
+                            <div
+                                key={index}
+                                className="flex justify-between items-center gap-4 flex-nowrap break-words"
+                            >
+                                <img
+                                    src={gericht.imgSrc}
+                                    alt={gericht.name}
+                                    className="w-28 h-28 rounded-lg object-cover"
+                                />
+                                <label className="text-md">
+                                    {gericht.name}
+                                </label>
+                                <input
+                                    type="tel"
+                                    className="w-14 h-14 rounded-lg pl-3"
+                                    value={bestellungen[index].anzahl}
+                                    placeholder="0"
+                                    onChange={(e) =>
+                                        handleAnzahlAendern(
+                                            index,
+                                            Number(e.target.value)
+                                        )
+                                    }
+                                />
+                            </div>
+                        ))}
+                    </div>
 
-                <div className="flex justify-center">
-                    <Button className="w-96" type="submit">
-                        Senden
-                    </Button>
-                </div>
-            </form>
-        </Form>
+                    <div className="flex justify-center">
+                        <Button className="w-96" type="submit">
+                            Senden
+                        </Button>
+                    </div>
+                </form>
+            </Form>
+            <Toaster />
+        </>
     );
 }
