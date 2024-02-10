7fca7234b1df3852968409fb72457a4a022f340e
herbertnikolajewskidci <herbert.nikolajewski@digitalcareerinstitute.org>
2024-02-10 01:16:49 +0100

    Aktualisiere Header2, Info, Footer, Faq, Response und Tagesablauf Komponenten

--- src/components/react/CountdownReact.tsx
+++ src/components/react/CountdownReact.tsx
@@ -1,49 +1,58 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const CountdownReact = () => {
-  const targetDate = new Date('2024-05-11T12:00:00');
+    const targetDate = new Date("2024-05-11T13:00:00");
 
-const calculateTimeLeft = () => {
-    const now = new Date();
-    const difference = Number(targetDate) - Number(now);
+    const calculateTimeLeft = () => {
+        const now = new Date();
+        const difference = Number(targetDate) - Number(now);
 
-    if (difference > 0) {
-        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-        
-        return { days, hours, minutes };
-    } else {
-        return { days: 0, hours: 0, minutes: 0 };
-    }
-};
+        if (difference > 0) {
+            const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+            const hours = Math.floor(
+                (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+            );
+            const minutes = Math.floor(
+                (difference % (1000 * 60 * 60)) / (1000 * 60)
+            );
 
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+            return { days, hours, minutes };
+        } else {
+            return { days: 0, hours: 0, minutes: 0 };
+        }
+    };
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
-    return () => clearInterval(timer);
-  }, []);
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
 
-  const units = [
-    { label: 'Tage', value: timeLeft.days },
-    { label: 'Stunden', value: timeLeft.hours },
-    { label: 'Minuten', value: timeLeft.minutes },
-  ];
+        return () => clearInterval(timer);
+    }, []);
 
-  return (
-    <div className="mt-4 flex justify-around items-center">
-      {units.map((unit, index) => (
-        <div key={index} className="h-20 flex flex-col justify-around items-center">
-          <span className="text-3xl countdown-item">{unit.value}</span>
-          <span className="font-yuliya text-3xl">{unit.label}</span>
+    const units = [
+        { label: "Tage", value: timeLeft.days },
+        { label: "Stunden", value: timeLeft.hours },
+        { label: "Minuten", value: timeLeft.minutes },
+    ];
+
+    return (
+        <div className="mt-4 max-w-md mx-auto flex justify-around items-center">
+            {units.map((unit, index) => (
+                <div
+                    key={index}
+                    className="h-20 flex flex-col justify-around items-center"
+                >
+                    <span className="text-3xl countdown-item">
+                        {unit.value}
+                    </span>
+                    <span className="font-yuliya text-3xl">{unit.label}</span>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default CountdownReact;
